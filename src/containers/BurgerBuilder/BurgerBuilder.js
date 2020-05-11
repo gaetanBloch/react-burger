417018624a6b4302fe987345651f18cee16293df
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-11 23:03:43 +0200

    Added Ingredient prices and totalPrice in the state

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,6 +3,13 @@ import Aux from '../../hoc/ReactAux';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
+const INGREDIENT_PRICES = {
+  salad: 0.5,
+  cheese: 1,
+  meat: 2,
+  bacon: 1
+}
+
 class BurgerBuilder extends Component {
   state = {
     ingredients: {
@@ -10,7 +17,8 @@ class BurgerBuilder extends Component {
       bacon: 0,
       cheese: 0,
       meat: 0
-    }
+    },
+    totalPrice: 2
   }
 
   addIngredientHandler = (type) => {
