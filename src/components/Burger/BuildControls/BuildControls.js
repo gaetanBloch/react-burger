88337d6601724627caed88a622e55706904c7167
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-11 22:48:48 +0200

    Added BuildControl Components inside the BuildControls Component

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -1,10 +1,20 @@
 import React from 'react';
 
 import styles from './BuildControls.module.css'
+import BuildControl from './BuildControl/BuildControl';
+
+const controls = [
+  {label: 'Salad', type: 'salad'},
+  {label: 'Bacon', type: 'bacon'},
+  {label: 'Cheese', type: 'cheese'},
+  {label: 'Meat', type: 'meat'}
+];
 
 const BuildControls = (props) => (
   <div className={styles.BuildControls}>
-    
+    {controls.map(control => {
+      return <BuildControl key={control.label} label={control.label} />
+    })}
   </div>
 );
 
