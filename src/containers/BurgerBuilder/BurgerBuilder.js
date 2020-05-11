1d451e8d01309dc56406e0849052870de4cb6330
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-11 23:01:47 +0200

    Create method AddIngredientHandler to add an ingredient

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -6,12 +6,20 @@ import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 class BurgerBuilder extends Component {
   state = {
     ingredients: {
-      salad: 1,
-      bacon: 1,
-      cheese: 2,
-      meat: 2
+      salad: 0,
+      bacon: 0,
+      cheese: 0,
+      meat: 0
     }
   }
+
+  addIngredientHandler = (type) => {
+    const oldCount = this.state.ingredients[type];
+    const updatedCount = oldCount + 1;
+    const updatedIngredients = {...this.state.ingredients};
+    updatedIngredients[type] = updatedCount;
+    this.setState({ingredients: updatedIngredients});
+  };
 
   render() {
     return (
