9f727d1cf5209350a0656378ca7439d47ad39819
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-12 15:40:49 +0200

    Added NavigationItem Component PropTypes configuration

--- src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -1,7 +1,20 @@
 import React from 'react';
 
+import PropTypes from 'prop-types'
+
+import styles from './NavigationItem.module.css'
+
 const NavigationItem = (props) => (
-  <li><a href="/">A Link</a></li>
+  <li className={styles.NavigationItem}>
+    <a href={props.link} className={props.active ? styles.active : null}>
+      {props.children}
+    </a>
+  </li>
 );
+
+NavigationItem.propTypes = {
+  link: PropTypes.string.isRequired,
+  active: PropTypes.bool
+}
 
 export default NavigationItem;
