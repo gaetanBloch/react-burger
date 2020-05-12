5da4b101412702e9adbded8b4048cc86f2880c45
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-12 15:44:10 +0200

    Added Navigation Items in the Toolbar

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
 import styles from './NavigationItems.module.css'
+import NavigationItem from './NavigationItem/NavigationItem';
 
 const NavigationItems = (props) => (
   <ul className={styles.NavigationItems}>
-
+    <NavigationItem link="/" active>Burger Builder</NavigationItem>
+    <NavigationItem link="/">Checkout</NavigationItem>
   </ul>
 );
 
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -2,13 +2,14 @@ import React from 'react';
 
 import styles from './Toolbar.module.css'
 import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
 
 const Toolbar = (props) => (
   <header role="navigation" className={styles.Toolbar}>
     <div>MENU</div>
     <Logo />
     <nav>
-      ...
+      <NavigationItems />
     </nav>
   </header>
 );
