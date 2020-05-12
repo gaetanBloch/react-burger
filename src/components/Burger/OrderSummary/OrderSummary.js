6bf30d2f85e0c594aa7c7de1255153164429b996
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-12 13:43:44 +0200

    Display the total price in the Order Summary

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -20,6 +20,7 @@ const OrderSummary = (props) => {
       <ul>
         {ingredientSummary}
       </ul>
+      <p><strong>Total Price: ${props.price.toFixed(2)}</strong></p>
       <p>Continue to checkout ?</p>
       <Button buttonType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
       <Button buttonType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
