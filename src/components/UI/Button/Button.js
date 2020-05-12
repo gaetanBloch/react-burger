58ca7eb51ec6355551412cd227e1ada8f1068d49
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-12 13:07:08 +0200

    Added PropTypes configuration to the button

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import PropTypes from 'prop-types'
 
 import styles from './Button.module.css'
+import BurgerIngredient from '../../Burger/BurgerIngredient/BurgerIngredient';
 
 const Button = (props) => (
   <button className={[styles.Button, styles[props.buttonType]].join(' ')} onClick={props.clicked}>
     {props.children}
   </button>
 );
+
+BurgerIngredient.propTypes = {
+  buttonType: PropTypes.string.isRequired,
+  clicked: PropTypes.func.isRequired
+}
 
 export default Button;
