d005351df491cdc387c01e8b7f424c7425c4281b
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-12 19:56:53 +0200

    Improved performance by preventing the update of Modal content when the modal is not shown

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,23 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types'
 
 import styles from './Modal.module.css'
 import Aux from '../../../hoc/ReactAux';
 import Backdrop from '../Backdrop/Backdrop';
 
-const Modal = (props) => (
-  <Aux>
-    <Backdrop show={props.show} clicked={props.modalClosed} />
-    <div
-      className={styles.Modal}
-      style={{
-        transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-        opacity: props.show ? 1 : 0
-      }}>
-      {props.children}
-    </div>
-  </Aux>
-);
+class Modal extends Component {
+  shouldComponentUpdate(nextProps, nextState, nextContext) {
+    return nextProps.show !== this.props.show
+  }
+
+  render() {
+    return (
+      <Aux>
+        <Backdrop show={this.props.show} clicked={this.props.modalClosed} />
+        <div
+          className={styles.Modal}
+          style={{
+            transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+            opacity: this.props.show ? 1 : 0
+          }}>
+          {this.props.children}
+        </div>
+      </Aux>
+    );
+  }
+}
 
 Modal.propTypes = {
   show: PropTypes.bool.isRequired,
