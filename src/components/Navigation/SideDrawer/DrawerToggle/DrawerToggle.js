1c99bffc626b290c2a7b68207ab69e06e3c633cd
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-12 19:19:09 +0200

    Added a Hamburger toggle icon

--- src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
+++ src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types'
 
+import styles from './DrawerToggle.module.css'
+
 const DrawerToggle = (props) => (
-  <button onClick={props.clicked}>Toggle</button>
+  <div className={styles.DrawerToggle} onClick={props.clicked}>
+    <div></div>
+    <div></div>
+    <div></div>
+  </div>
 );
 
 DrawerToggle.propTypes = {
