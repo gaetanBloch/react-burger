fb71e626d18c80954b1d0c9a430886704512da00
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-12 15:00:17 +0200

    Created NavigationItems Component

---
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import styles from './NavigationItems.module.css'
+
+const NavigationItems = (props) => (
+  <ul className={styles.NavigationItems}>
+
+  </ul>
+);
+
+export default NavigationItems;
