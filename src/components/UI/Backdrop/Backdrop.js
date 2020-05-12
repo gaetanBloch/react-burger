e05a80d438c708d072a682d4c66a4df7bb7b3475
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-12 12:52:45 +0200

    Close the Modal when clicking on the Backdrop

--- src/components/UI/Backdrop/Backdrop.js
+++ src/components/UI/Backdrop/Backdrop.js
@@ -3,7 +3,7 @@ import React from 'react';
 import styles from './Backdrop.module.css'
 
 const Backdrop = (props) => (
-  props.show ? <div className={styles.Backdrop}></div> : null
+  props.show ? <div className={styles.Backdrop} onClick={props.clicked}></div> : null
 );
 
 export default Backdrop;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -87,6 +87,10 @@ class BurgerBuilder extends Component {
     this.setState({purchasing: true});
   }
 
+  purchaseCanceledHandler = () => {
+    this.setState({purchasing: false});
+  }
+
   render() {
     const disabledInfo = {...this.state.ingredients};
     Object.keys(disabledInfo).forEach(key => {
@@ -95,7 +99,7 @@ class BurgerBuilder extends Component {
 
     return (
       <Aux>
-        <Modal show={this.state.purchasing}>
+        <Modal show={this.state.purchasing} modalClosed={this.purchaseCanceledHandler}>
           <OrderSummary ingredients={this.state.ingredients} />
         </Modal>
         <Burger ingredients={this.state.ingredients} />
