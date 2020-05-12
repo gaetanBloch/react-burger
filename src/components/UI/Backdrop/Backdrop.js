85fa53aa8cd4330dd762578bc303c960e7d6b617
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-12 13:14:09 +0200

    Added PropTypes configuration to the Backdrop

--- src/components/UI/Backdrop/Backdrop.js
+++ src/components/UI/Backdrop/Backdrop.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import PropTypes from 'prop-types'
 
 import styles from './Backdrop.module.css'
 
 const Backdrop = (props) => (
   props.show ? <div className={styles.Backdrop} onClick={props.clicked}></div> : null
 );
+
+Backdrop.propTypes = {
+  clicked: PropTypes.func
+}
 
 export default Backdrop;
