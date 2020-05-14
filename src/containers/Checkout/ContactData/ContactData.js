1a9acf1f412272bec0c943af4dc08fbf9885de04
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-14 22:24:00 +0200

    Added ContactData Container Component with its state

---
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class ContactData extends Component {
+  state = {
+    name: '',
+    email: '',
+    address: {
+      street: '',
+      postalCode: ''
+    }
+  };
+
+  render() {
+    return ();
+  }
+}
+
+export default ContactData;
