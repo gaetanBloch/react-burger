9d333d94d877f61179b6ce697fdaabf7975166f9
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-15 00:20:25 +0200

    Implemented Navigation Links to Burger Builder and Orders

--- src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -2,12 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types'
 
 import styles from './NavigationItem.module.css'
+import { NavLink } from 'react-router-dom';
 
 const NavigationItem = (props) => (
   <li className={styles.NavigationItem}>
-    <a href={props.link} className={props.active ? styles.active : null}>
+    <NavLink to={props.link} activeClassName={styles.active} exact={props.exact}>
       {props.children}
-    </a>
+    </NavLink>
   </li>
 );
 
