1c19419d536e775d9d8e392a4d2118048930a537
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-15 00:07:12 +0200

    Added Content and Styling to the Order Component

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
+import styles from './Order.module.css'
+
 const Order = (props) => (
-  
+  <div className={styles.Order}>
+    <p>Ingredients: Salad (1)</p>
+    <p>Price: <strong>$6.50</strong></p>
+  </div>
 );
 
 export default Order;
