7901b15b6087107702bd3b1429ab6dcbbac0fee5
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-15 00:09:21 +0200

    Added Order Components in the Orders Component

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
+import Order from '../../components/Order/Order';
 
 class Orders extends Component {
   render() {
-    return ();
+    return (
+      <div>
+        <Order />
+        <Order />
+      </div>
+    );
   }
 }
 
