1ba2a3c4f7dff961cfcbefd867bcac02c961bf2a
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-15 00:42:17 +0200

    Get the orders from the server

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,7 +1,29 @@
 import React, { Component } from 'react';
+
+import axios from '../../axios-orders'
 import Order from '../../components/Order/Order';
 
 class Orders extends Component {
+  state = {
+    orders: []
+  }
+
+  componentDidMount() {
+    this.getOrders();
+  }
+
+  getOrders = async () => {
+    const response = await axios.get('/orders');
+    const fetchedOrders = [];
+    Object.keys(response.data).forEach(key => {
+      fetchedOrders.push({
+        ...response.data[key],
+        id: key
+      });
+    })
+    this.setState({orders: fetchedOrders});
+  }
+
   render() {
     return (
       <div>
