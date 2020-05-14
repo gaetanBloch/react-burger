423fa2478b35027fd9d89fadc967d3919c2e5121
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-15 01:33:23 +0200

    Display the orders from the server

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -3,12 +3,31 @@ import PropTypes from 'prop-types';
 
 import styles from './Order.module.css'
 
-const Order = (props) => (
-  <div className={styles.Order}>
-    <p>Ingredients: Salad (1)</p>
-    <p>Price: <strong>$6.50</strong></p>
-  </div>
-);
+const Order = (props) => {
+    const ingredients = [];
+    Object.keys(props.ingredients).forEach(ingredientName => {
+      ingredients.push({
+        name: ingredientName === 'alad' ? 'salad' : ingredientName,
+        amount: props.ingredients[ingredientName]
+      });
+    });
+
+    const ingredientOutput = ingredients.map(ingredient => {
+      return <span
+        key={ingredient.name}
+        className={styles.Ingredient}>
+        {ingredient.name} ({ingredient.amount})
+      </span>
+    })
+
+    return (
+      <div className={styles.Order}>
+        <p>Ingredients: {ingredientOutput}</p>
+        <p>Price: <strong>${props.price.toFixed(2)}</strong></p>
+      </div>
+    )
+  }
+;
 
 Order.propTypes = {
   ingredients: PropTypes.object.isRequired,
