701ba3fdf4451bde5fb697d66396946e9ddea8c5
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-14 18:36:56 +0200

    Added PropTypes to CheckoutSummary Component and added content with the Burger

--- src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const CheckoutSummary = (props) => (
+import styles from './CheckoutSummary.module.css'
+import Burger from '../../Burger/Burger';
 
-);
+const CheckoutSummary = (props) => {
+  return (
+    <div className={styles.CheckoutSummary}>
+      <h1>We hope it tasted well!</h1>
+      <div className={styles.Burger}>
+        <Burger ingredients={props.ingredients} />
+      </div>
+    </div>
+  );
+};
+
+CheckoutSummary.propTypes = {
+  ingredients: PropTypes.array.isRequired
+}
 
 export default CheckoutSummary;
