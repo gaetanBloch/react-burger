4f5bd523cca8785d4734b57b3315eff6e506fb37
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-15 10:19:06 +0200

    Dynamically create the input in the InputComponent

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -1,10 +1,33 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import styles from './Input.module.css'
 
-const Input = (props) => (
-  <div className={styles.Input}>
-  </div>
-);
+const Input = (props) => {
+  let inputElement = null;
+  switch (props.inputElement) {
+    case 'input':
+      inputElement = <input id={props.id} {...props} />
+      break;
+    case 'textarea':
+      inputElement = <textarea id={props.id} {...props} />
+      break;
+    default:
+      inputElement = <input id={props.id} {...props} />
+  }
+
+  return (
+    <div className={styles.Input}>
+      <label htmlFor={props.id}>{props.label}</label>
+      {inputElement}
+    </div>
+  );
+};
+
+Input.propTypes = {
+  label: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
+  inputElement: PropTypes.string
+}
 
 export default Input;
