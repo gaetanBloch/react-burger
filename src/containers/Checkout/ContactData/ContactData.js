bd522041e4b79df8424a5b384b603991374a5a13
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-15 14:44:12 +0200

    Fixed common Validation Gotcha

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -46,15 +46,15 @@ class ContactData extends Component {
   };
 
   checkValidity(value, rules) {
-    let isValid = false;
+    let isValid = true;
     if (rules.required) {
-      isValid = value !== null && value !== undefined && value.trim() !== '';
+      isValid = value != null && value.trim() !== '' && isValid;
     }
     if (rules.minLength) {
-      isValid = value >= rules.minLength;
+      isValid = value != null && value.length >= rules.minLength && isValid;
     }
     if (rules.maxLength) {
-      isValid = value <= rules.maxLength;
+      isValid = value != null && value.length <= rules.maxLength && isValid;
     }
     return isValid;
   }
