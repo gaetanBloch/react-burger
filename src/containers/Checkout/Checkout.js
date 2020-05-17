0b5487e5b82e6a2d5f1fc6752fd2cd4f8fd55ace
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-17 11:37:33 +0200

    Redirect from the checkout page when no ingredients

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import { Route } from 'react-router';
+import React, { Component, Fragment } from 'react';
+import { Redirect, Route } from 'react-router';
 import { connect } from 'react-redux';
 
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
@@ -19,15 +19,20 @@ class Checkout extends Component {
   }
 
   render() {
-    return (
-      <div>
-        <CheckoutSummary
-          ingredients={this.props.ingredients}
-          checkoutCancelled={this.checkoutCancelledHandler}
-          checkoutContinued={this.checkoutContinuedHandler} />
-        <Route path={this.getContactDataPath()} component={ContactData} />
-      </div>
-    );
+    let summary = <Redirect to="/" />;
+    if (this.props.ingredients) {
+      summary = (
+        <Fragment>
+          <CheckoutSummary
+            ingredients={this.props.ingredients}
+            checkoutCancelled={this.checkoutCancelledHandler}
+            checkoutContinued={this.checkoutContinuedHandler} />
+          <Route path={this.getContactDataPath()} component={ContactData} />
+        </Fragment>
+      )
+    }
+
+    return summary;
   }
 }
 
