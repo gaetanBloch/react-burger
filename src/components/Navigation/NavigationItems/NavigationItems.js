a1a52f09ffccdb80828782a84674a72440360e52
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-17 18:15:24 +0200

    Added navigation link to Auth Page

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import styles from './NavigationItems.module.css'
+import styles from './NavigationItems.module.css';
 import NavigationItem from './NavigationItem/NavigationItem';
 
 const NavigationItems = (props) => (
@@ -8,6 +8,7 @@ const NavigationItems = (props) => (
     <ul className={styles.NavigationItems}>
       <NavigationItem link="/" exact>Burger Builder</NavigationItem>
       <NavigationItem link="/orders">Orders</NavigationItem>
+      <NavigationItem link="/auth">Authenticate</NavigationItem>
     </ul>
   </nav>
 );
