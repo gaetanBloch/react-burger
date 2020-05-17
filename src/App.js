640603a9042cc6c892a23f685a19359b25bccf40
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-17 18:03:42 +0200

    Added Auth Route

--- src/App.js
+++ src/App.js
@@ -6,20 +6,22 @@ import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
 import Orders from './containers/Orders/Orders';
+import Auth from './containers/Auth/Auth';
 
 class App extends Component {
-  render() {
+  render () {
     return (
       <div className={styles.App}>
         <Layout>
           <Switch>
             <Route path="/checkout" component={Checkout} />
             <Route path="/orders" component={Orders} />
+            <Route path="/auth" component={Auth} />
             <Route path="/" component={BurgerBuilder} exact />
           </Switch>
         </Layout>
       </div>
-    )
+    );
   }
 }
 
