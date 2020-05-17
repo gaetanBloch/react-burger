54a9a7221851683389b2f2f980d9267ad5b4ff31
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-17 15:27:27 +0200

    Made the Burger Builder Reducer leaner by using external methods

--- src/store/reducers/burgerBuilder.js
+++ src/store/reducers/burgerBuilder.js
@@ -1,11 +1,11 @@
 import * as actionTypes from '../actions/actionTypes'
-import { updateObject } from '../utils';
+import { updateObject } from '../utils'
 
 const INGREDIENT_PRICES = {
   salad: 0.5,
   bacon: 1,
   cheese: 1,
-  meat: 2
+  meat: 2,
 }
 
 const INITIAL_BURGER_PRICE = 2
@@ -13,59 +13,71 @@ const INITIAL_BURGER_PRICE = 2
 const initialState = {
   ingredients: null,
   totalPrice: 0,
-  error: false
+  error: false,
+}
+
+const addIngredient = (state, action) => {
+  const updatedIngredient = {
+    [action.ingredientType]: state.ingredients[action.ingredientType] + 1,
+  }
+  const updatedIngredients = updateObject(state.ingredients, updatedIngredient)
+  const updatedState = {
+    ingredients: updatedIngredients,
+    totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientType],
+  }
+  return updateObject(state, updatedState)
+}
+
+const removeIngredient = (state, action) => {
+  const oldCount = state.ingredients[action.ingredientType]
+  if (oldCount > 0) {
+    const updatedIngredient = {
+      [action.ingredientType]: oldCount - 1,
+    }
+    const updatedIngredients = updateObject(state.ingredients,
+      updatedIngredient)
+    const updatedState = {
+      ingredients: updatedIngredients,
+      totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientType],
+    }
+    return updateObject(state, updatedState)
+  }
+  return state
+}
+
+const fetchIngredientSuccess = (state, action) => {
+  const calculateInitialPrice = (ingredients) => {
+    const totalPrice = Object.keys(ingredients)
+      .map(key => ingredients[key] * INGREDIENT_PRICES[key])
+      .reduce((sum, price) => sum + price)
+    return INITIAL_BURGER_PRICE + totalPrice
+  }
+
+  return updateObject(state, {
+    ingredients: {
+      salad: action.ingredients.salad,
+      bacon: action.ingredients.bacon,
+      cheese: action.ingredients.cheese,
+      meat: action.ingredients.meat,
+    },
+    totalPrice: calculateInitialPrice(action.ingredients),
+    error: false,
+  })
 }
 
 const burgerBuilder = (state = initialState, action) => {
   switch (action.type) {
-    case actionTypes.ADD_INGREDIENT: {
-      const updatedIngredient = {
-        [action.ingredientType]: state.ingredients[action.ingredientType] + 1
-      };
-      const updatedIngredients = updateObject(state.ingredients, updatedIngredient);
-      const updatedState = {
-        ingredients: updatedIngredients,
-        totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientType]
-      }
-      return updateObject(state, updatedState);
-    }
-    case actionTypes.REMOVE_INGREDIENT: {
-      const oldCount = state.ingredients[action.ingredientType]
-      if (oldCount > 0) {
-        const updatedIngredient = {
-          [action.ingredientType]: oldCount - 1
-        };
-        const updatedIngredients = updateObject(state.ingredients, updatedIngredient);
-        const updatedState = {
-          ingredients: updatedIngredients,
-          totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientType]
-        }
-        return updateObject(state, updatedState);
-      }
-      return state;
-    }
+    case actionTypes.ADD_INGREDIENT:
+      return addIngredient(state, action)
+    case actionTypes.REMOVE_INGREDIENT:
+      return removeIngredient(state, action)
     case actionTypes.FETCH_INGREDIENTS_SUCCESS:
-      const calculateInitialPrice = (ingredients) => {
-        const totalPrice = Object.keys(ingredients)
-          .map(key => ingredients[key] * INGREDIENT_PRICES[key])
-          .reduce((sum, price) => sum + price);
-        return INITIAL_BURGER_PRICE + totalPrice;
-      }
-      return updateObject(state, {
-        ingredients: {
-          salad: action.ingredients.salad,
-          bacon: action.ingredients.bacon,
-          cheese: action.ingredients.cheese,
-          meat: action.ingredients.meat,
-        },
-        totalPrice: calculateInitialPrice(action.ingredients),
-        error: false
-      });
+      return fetchIngredientSuccess(state, action)
     case actionTypes.FETCH_INGREDIENTS_FAIL:
-      return updateObject(state, {error: true})
+      return updateObject(state, { error: true })
     default:
-      return state;
+      return state
   }
 }
 
-export default burgerBuilder;
+export default burgerBuilder
--- src/store/reducers/order.js
+++ src/store/reducers/order.js
@@ -1,36 +1,36 @@
 import * as actionsTypes from '../actions/actionTypes'
-import { updateObject } from '../utils';
+import { updateObject } from '../utils'
 
 const initialState = {
   orders: [],
   loading: false,
-  purchased: false
+  purchased: false,
 }
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case actionsTypes.PURCHASE_INIT:
-      return updateObject(state, {purchased: false});
+      return updateObject(state, { purchased: false })
     case actionsTypes.PURCHASE_BURGER_START:
-      return updateObject(state, {loading: true});
+      return updateObject(state, { loading: true })
     case actionsTypes.PURCHASE_BURGER_SUCCESS:
-      const newOrder = updateObject(action.orderData, {id: action.orderId});
+      const newOrder = updateObject(action.orderData, { id: action.orderId })
       return updateObject(state, {
         orders: state.orders.concat(newOrder),
         purchased: true,
-        loading: false
-      });
+        loading: false,
+      })
     case actionsTypes.PURCHASE_BURGER_FAIL:
-      return updateObject(state, {loading: false});
+      return updateObject(state, { loading: false })
     case actionsTypes.FETCH_ORDERS_START :
-      return updateObject(state, {loading: true});
+      return updateObject(state, { loading: true })
     case actionsTypes.FETCH_ORDERS_SUCCESS :
-      return updateObject(state, {orders: action.orders, loading: false});
+      return updateObject(state, { orders: action.orders, loading: false })
     case actionsTypes.FETCH_ORDERS_FAIL :
-      return updateObject(state, {loading: false});
+      return updateObject(state, { loading: false })
     default:
-      return state;
+      return state
   }
 }
 
-export default reducer;
+export default reducer
