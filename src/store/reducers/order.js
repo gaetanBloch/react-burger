5bb75ee8a2d3b69574281b292f74bb068acd7dc3
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-17 11:04:52 +0200

    Handle Purchase Burger Start in the reducer

--- src/store/reducers/order.js
+++ src/store/reducers/order.js
@@ -7,6 +7,11 @@ const initialState = {
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
+    case actionsTypes.PURCHASE_BURGER_START:
+      return {
+        ...state,
+        loading: true
+      }
     case actionsTypes.PURCHASE_BURGER_SUCCESS:
       return {
         ...state,
