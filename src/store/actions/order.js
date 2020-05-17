797fe6a1d9ec5341c3a64aafe702566de00b70bd
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-17 13:36:03 +0200

    Added Orders Action Creators

--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -1,6 +1,8 @@
 import * as actionTypes from './actionTypes';
 import axios from '../../axios-orders';
 
+/* ******** PURCHASE BURGER ******* */
+
 const purchaseBurgerSuccess = (orderId, orderData) => {
   return {
     type: actionTypes.PURCHASE_BURGER_SUCCESS,
@@ -40,6 +42,50 @@ export const purchaseBurger = (orderData) => {
 export const purchaseInit = () => {
   return {
     type: actionTypes.PURCHASE_INIT
+  }
+}
+
+/* ******** FETCH ORDERS ******* */
+
+const fetchOrdersSuccess = (orders) => {
+  return {
+    type: actionTypes.FETCH_ORDERS_START,
+    orders: orders
+  }
+}
+
+const fetchOrdersFail = () => {
+  return {
+    type: actionTypes.FETCH_ORDERS_FAIL
+  }
+}
+
+const getOrders = async (dispatch) => {
+  try {
+    const response = await axios.get('/orders.json');
+    const fetchedOrders = [];
+    Object.keys(response.data).forEach(key => {
+      fetchedOrders.push({
+        ...response.data[key],
+        id: key
+      });
+    })
+    dispatch(fetchOrdersSuccess(fetchedOrders));
+  } catch (error) {
+    dispatch(fetchOrdersFail());
+  }
+}
+
+const fetchOrdersStart = () => {
+  return {
+    type: actionTypes.FETCH_ORDERS_START
+  }
+}
+
+export const fetchOrders = () => {
+  return dispatch => {
+    dispatch(fetchOrdersStart());
+    getOrders(dispatch);
   }
 }
 
