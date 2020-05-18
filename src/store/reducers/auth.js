7b6294b547d4a0776d1961e2bbc26765d0ac01cc
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-18 12:09:21 +0200

    Handle Auth Success in Auth Reducer

--- src/store/reducers/auth.js
+++ src/store/reducers/auth.js
@@ -12,10 +12,21 @@ const authStart = (state) => {
   return updateObject(state, { error: null, loading: true });
 };
 
+const authSuccess = (state, action) => {
+  return updateObject(state, {
+    token: action.token,
+    userId: action.userId,
+    error: null,
+    loading: false
+  });
+};
+
 const reducer = (state, action) => {
   switch (action.type) {
     case actionTypes.AUTH_START:
       return authStart(state);
+    case actionTypes.AUTH_SUCCESS:
+      return authSuccess(state, action);
     default:
       return state;
   }
