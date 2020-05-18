9f2aa8a97d8fa7961a84d75649d8cdd51c2f8584
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-18 12:05:57 +0200

    Handle Auth Start in Auth reducer

--- src/store/reducers/auth.js
+++ src/store/reducers/auth.js
@@ -1,7 +1,24 @@
-const initialState = {};
+import * as actionTypes from '../actions/actionTypes';
+import { updateObject } from '../storeUtils';
+
+const initialState = {
+  token: null,
+  userId: null,
+  error: null,
+  loading: false
+};
+
+const authStart = (state) => {
+  return updateObject(state, { error: null, loading: true });
+};
 
 const reducer = (state, action) => {
-
+  switch (action.type) {
+    case actionTypes.AUTH_START:
+      return authStart(state);
+    default:
+      return state;
+  }
 };
 
 export default reducer;
