50441720c1b9e8eccc8f977d1dbe3d8203848705
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-19 20:04:32 +0200

    Added Test for the auth reducer to check the authentication success action

--- src/store/reducers/auth.js
+++ src/store/reducers/auth.js
@@ -1,7 +1,7 @@
 import * as actionTypes from '../actions/actionTypes';
 import { updateObject } from '../../shared/utils';
 
-const initialState = {
+export const initialState = {
   token: null,
   userId: null,
   error: null,
--- src/store/reducers/auth.test.js
+++ src/store/reducers/auth.test.js
@@ -1,14 +1,20 @@
 import reducer from './auth';
 import * as actionTypes from '../actions/actionTypes';
+import { initialState } from './auth';
 
 describe('Auth reducer', () => {
   it('should return the initial state', () => {
-    expect(reducer(undefined, {})).toEqual({
-      token: null,
-      userId: null,
-      error: null,
-      loading: false,
-      authRedirectPath: '/'
-    })
+    expect(reducer(initialState, {})).toEqual(initialState);
   });
+
+  it('should store the token upon authentication', () => {
+    const updatedStateProps = {
+      token: 'some-token',
+      userId: 'some-user-id'
+    }
+    expect(reducer(initialState, {
+      type: actionTypes.AUTH_SUCCESS,
+      ...updatedStateProps
+    })).toEqual({...initialState, ...updatedStateProps});
+  })
 });
