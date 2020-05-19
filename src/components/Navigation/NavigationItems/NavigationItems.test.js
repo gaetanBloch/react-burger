ae9c8c15ef5d37175f5217f71d7671709e6600a2
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-19 18:21:31 +0200

    Added another NavigationItems test

--- src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -13,4 +13,10 @@ describe('<NavigationItems />', () => {
       const wrapper = shallow(<NavigationItems />);
       expect(wrapper.find(NavigationItem)).toHaveLength(2);
     });
+
+  it('should render three <NavigationItem /> components if not authenticated',
+    () => {
+      const wrapper = shallow(<NavigationItems isAuthenticated />);
+      expect(wrapper.find(NavigationItem)).toHaveLength(3);
+    });
 });
