0443d414342a3ed294ea5bf05e84455cca68c7bf
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-22 23:07:26 +0200

    Converted Modal Class Component into a Functional Component

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,34 +1,33 @@
-import React, { Component, Fragment } from 'react';
-import PropTypes from 'prop-types'
+import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
 
-import styles from './Modal.module.css'
+import styles from './Modal.module.css';
 import Backdrop from '../Backdrop/Backdrop';
 
-class Modal extends Component {
-  shouldComponentUpdate(nextProps, nextState, nextContext) {
-    return nextProps.show !== this.props.show || nextProps.children !== this.props.children
-  }
+const Modal = props => {
+  // const shouldComponentUpdate = (nextProps, nextState, nextContext) => {
+  //   return nextProps.show !== this.props.show
+  //     || nextProps.children !== this.props.children;
+  // };
 
-  render() {
-    return (
-      <Fragment>
-        <Backdrop show={this.props.show} clicked={this.props.modalClosed} />
-        <div
-          className={styles.Modal}
-          style={{
-            transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
-            opacity: this.props.show ? 1 : 0
-          }}>
-          {this.props.children}
-        </div>
-      </Fragment>
-    );
-  }
-}
+  return (
+    <Fragment>
+      <Backdrop show={props.show} clicked={props.modalClosed} />
+      <div
+        className={styles.Modal}
+        style={{
+          transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
+          opacity: props.show ? 1 : 0
+        }}>
+        {props.children}
+      </div>
+    </Fragment>
+  );
+};
 
 Modal.propTypes = {
   show: PropTypes.bool.isRequired,
   modalClosed: PropTypes.func.isRequired
-}
+};
 
 export default Modal;
